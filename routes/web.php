47c91295462b8f86f618aae2dfa48b56df207e3e
GaniViola <ganiprasetyo711.@gmail.com>
2025-04-23 09:16:58 +0700

    admin template

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,3 @@ Route::get('/', function () {
 Route::get('/secure-area/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/secure-area/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-// coba
-Route::get('/cobasb', function() {
-    return view('admin.layouts.app');
-});
