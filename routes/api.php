fc77d36e6cf975960b3233bb0b99fa1102e5b459
GaniViola <ganiprasetyo711.@gmail.com>
2025-04-27 12:54:04 +0700

    Api category

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\CategoryController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +17,5 @@ Route::post('login', [ApiController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [ApiController::class, 'logout']);
 });
+
+Route::get('/categories', [CategoryController::class, 'index']);
